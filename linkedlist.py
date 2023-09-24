beb81b2bd2aa29aea915c43666111e398a00d015
headhunter_jr <mykola.anchiforov@lnu.edu.ua>
2023-09-24 13:27:23 +0300

    added validity checks to all functions

--- linkedlist.py
+++ linkedlist.py
@@ -24,55 +24,94 @@ class LinkedList:
             return int_validity(some_string)
 
     def input_list(self):
-        n = int(input("Enter the number of elements: "))
-        for _ in range(n):
+        n = int(input("Enter the number of elements to add: "))
+        i = 0
+        while i < n:
             data = (input("Enter the element: "))
             if self.validity_check(data):
                 self.append(int(data))
+                i += 1
             else:
                 print("Invalid input. Please enter integer values.")
 
     def generate_list(self):
-        a = int(input("Enter the start of the range: "))
-        b = int(input("Enter the end of the range: "))
-        n = int(input("Enter the number of elements to generate: "))
-        for _ in range(n):
-            data = random.randint(a, b)
-            self.append(data)
+        while True:
+            a = input("Enter the start of the range: ")
+            b = input("Enter the end of the range: ")
+            n = input("Enter the number of elements to generate: ")
+            if not self.validity_check(a) or not self.validity_check(b) or not self.validity_check(n):
+                print("Invalid input. Please enter integer values.")
+                continue
+            a, b, n = int(a), int(b), int(n)
+            for _ in range(n):
+                data = random.randint(a, b)
+                self.append(data)
+            break
 
-    def insert(self, data, k):
-        new_node = Node(data)
-        if k == 0:
-            new_node.next = self.head
-            self.head = new_node
-            return
-        current = self.head
-        count = 0
-        while current and count < k - 1:
-            current = current.next
-            count += 1
-        if current is None:
-            print("Error. Current position does not exist in the list.")
-        else:
-            new_node.next = current.next
-            current.next = new_node
+    def insert(self):
+        while True:
+            data = input("Enter data for a new element: ")
+            k = input("Enter position for the new element: ")
 
-    def delete(self, k):
-        if k == 0:
-            if self.head:
-                self.head = self.head.next
+            if not self.validity_check(data) or not self.validity_check(k):
+                print("Invalid input. Please enter integer values.")
+                continue
+
+            data = int(data)
+            k = int(k)
+
+            new_node = Node(data)
+
+            if k == 0:
+                new_node.next = self.head
+                self.head = new_node
+                return
+
+            current = self.head
+            count = 0
+
+            while current and count < k - 1:
+                current = current.next
+                count += 1
+
+            if current is None:
+                print("Error. Current position does not exist in the list.")
+                continue
             else:
-                print("The list is empty")
-            return
-        current = self.head
-        count = 0
-        while current and count < k - 1:
-            current = current.next
-            count += 1
-        if current is None or current.next is None:
-            print("Error. Current position does not exist in the list.")
-        else:
-            current.next = current.next.next
+                new_node.next = current.next
+                current.next = new_node
+            break
+
+    def delete(self):
+        while True:
+            k = input("Enter position for the element to be deleted: ")
+
+            if not self.validity_check(k):
+                print("Invalid input. Please enter integer values.")
+                continue
+
+            k = int(k)
+
+            if k == 0:
+                if self.head:
+                    self.head = self.head.next
+                else:
+                    print("Error. The list is empty.")
+                break
+
+            current = self.head
+            count = 0
+
+            while current and count < k - 1:
+                current = current.next
+                count += 1
+
+            if current is None or current.next is None or k < 0:
+                print("Error. Current position does not exist in the list.")
+                continue
+            else:
+                current.next = current.next.next
+            break
 
     def display(self):
         current = self.head
--- task2.py
+++ task2.py
@@ -1,6 +1,12 @@
 from linkedlist import LinkedList
 if __name__ == "__main__":
     linked_list = LinkedList()
+    linked_list.generate_list()
+    linked_list.display()
+    linked_list.insert()
+    linked_list.display()
+    linked_list.delete()
+    linked_list.display()
     linked_list.input_list()
     linked_list.display()
     res = linked_list.calculate()
